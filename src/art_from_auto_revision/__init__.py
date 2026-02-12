4783250caf5eef69ac7a59185bee5c841b213630
--4444-j--99---- <padavano.anthony@gmail.com>
2026-02-12 08:29:03 -0500

    add package init

---
+++ src/art_from_auto_revision/__init__.py
@@ -0,0 +1,8 @@
+"""art-from-auto-revision: Governance as performance art.
+
+Interactive visualization of the 8-phase self-governing orchestration pipeline
+from auto-revision-epistemic-engine (ORGAN-I), rendered as generative visual art.
+"""
+
+__version__ = "0.1.0"
+__author__ = "organvm-ii-poiesis"
